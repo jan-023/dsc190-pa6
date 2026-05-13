442d78cff7466af165113b9aaa05f7a1af5cb4a4
Jacqueline Ng <jan023@ucsd.edu>
2026-05-12 18:58:29 -0700

    Implemented code and tests for 'today', 'yesterday', 'tomorrow'

--- src/nldate/parser.py
+++ src/nldate/parser.py
@@ -1,4 +1,16 @@
-from datetime import date
+from datetime import date, timedelta
+import re
 
 def parse(s: str, today: date | None = None) -> date:
-    return
+    if today is None:
+        today = date.today()
+
+    s = s.strip().lower()
+
+    if s == "today":
+        return today
+    if s == "yesterday":
+        return today - timedelta(days=1)
+    if s == "tomorrow":
+        return today + timedelta(days=1)
+
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,6 @@
 import pytest
 from nldate import parse
-from datetime import date
+from datetime import date, timedelta
 
 def test_no_args_provided():
     with pytest.raises(TypeError):
@@ -12,3 +12,11 @@ def test_today_date_provided():
 def test_today_date_not_provide():
     assert parse(s = "today") == date.today()
 
+def test_yesterday_date_provided():
+    assert parse(s = "yesterday", today = date(2024, 4, 1)) == date(2024, 3, 31)
+
+def test_yesterday_date_not_provided():
+    assert parse(s = "yesterday") == date.today() - timedelta(days = 1)
+
+def test_tomorrow_date_provided():
+    assert parse(s = "tomorrow", today = date(1998, 12, 31)) == date(1999, 1, 1)
