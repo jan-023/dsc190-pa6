01416699d190a44d436e1cfd86d16a5b6f57bcdd
Jacqueline Ng <jan023@ucsd.edu>
2026-05-12 18:34:59 -0700

    Added tests for 'today' arg

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,2 +1,14 @@
+import pytest
 from nldate import parse
 from datetime import date
+
+def test_no_args_provided():
+    with pytest.raises(TypeError):
+        parse()
+
+def test_today_date_provided():
+    assert parse(s = "today", today = date(2025, 5, 12)) == date(2025, 5, 12)
+
+def test_today_date_not_provide():
+    assert parse(s = "today") == date.today()
+
